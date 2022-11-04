3604f8f8f8b2a484de6842cad6e59ce902c2356a
Ky Bui Van <ky.buivan.dev@gmail.com>
2022-11-04 17:06:53 +0700

    Adding System Introspection

--- core/twin_core.cxx
+++ core/twin_core.cxx
@@ -1,10 +1,18 @@
 #include "twin_core.h"
+#include <iostream>
 #include <cmath>
 
 namespace core
 {
     float sqrt(float input)
     {
-        return std::sqrt(input);
+#if defined(HAVE_LOG) && defined(HAVE_EXP)
+    double result = std::exp(std::log(input) * 0.5);
+    std::cout << "Computing sqrt of " << input << " to be " << result
+                << " using log and exp" << std::endl;
+#else
+        double result = std::sqrt(result);
+#endif
+        return result;
     }
 }
