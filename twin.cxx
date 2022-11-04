29cb5993fb85e1a22456f5cfa5141099fad2a8e0
Ky Bui Van <ky.buivan.dev@gmail.com>
2022-11-04 11:19:58 +0700

    Adding a Version Number and Configured Header File

--- twin.cxx
+++ twin.cxx
@@ -1,8 +1,17 @@
 #include <iostream>
 #include <string>
+#include "twin_config.h"
 
 int main(int argc, char *argv[])
 {
+    if (argc < 2) {
+        // report version
+        std::cout << argv[0] << " Version " << twin_VERSION_MAJOR << "."
+                << twin_VERSION_MINOR << std::endl;
+        std::cout << "Usage: " << argv[0] << " number" << std::endl;
+        return 1;
+    }
+
     std::string orbits ("365.24 29.53");
     std::string::size_type sz;     // alias of size_t
 
