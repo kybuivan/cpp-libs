b7f7f20961b1f46964309c54d9cd9eb741e7d5a5
Ky Bui Van <ky.buivan.dev@gmail.com>
2022-11-04 14:41:18 +0700

    Install Rules

--- win.bat
+++ win.bat
@@ -1,4 +1,5 @@
 DEL /Q build\CMakeCache.txt
 cmake -S . -B ./build
-cmake --build ./build
+cmake --build ./build --config Debug
+cmake --install ./build --prefix /tmp/install-test --config Debug
 .\build\Release\twin.exe
