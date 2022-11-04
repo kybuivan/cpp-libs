257de35a48790ca39d283dc758fd4419b5ed150f
Ky Bui Van <ky.buivan.dev@gmail.com>
2022-11-04 16:36:50 +0700

    Adding Support for a Testing Dashboard

--- win.bat
+++ win.bat
@@ -3,5 +3,5 @@ cmake -S . -B ./build
 cmake --build ./build --config Debug
 cmake --install ./build --prefix /tmp/install-test --config Debug
 cd build
-ctest -C Debug
+ctest [-VV] -C Debug -D Experimental
 REM .\build\Debug\twin.exe
