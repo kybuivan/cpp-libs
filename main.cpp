1c3f68572f730e94e2e7ae9094863e243bec4d3b
Ky Bui Van <ky.buivan.dev@gmail.com>
2022-11-04 11:07:18 +0700

    Specifying the C++ Standard

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <string>
 
-int main()
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello" << std::endl; 
+    std::string orbits ("365.24 29.53");
+    std::string::size_type sz;     // alias of size_t
+
+    double earth = std::stod (orbits,&sz);
+    double moon = std::stod (orbits.substr(sz));
+    std::cout << "The moon completes " << (earth/moon) << " orbits per Earth year.\n";
     return 0;
 }
